328c100810b40992664b301db2e93d4e08a68010
Mario Toribio <yuniormtr@gmail.com>
2022-06-26 17:14:37 -0400

    1

--- TalkingMuch/settings.py
+++ TalkingMuch/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d(q&o$#w+kytao&@jumfxab8c8u*0xgagpgav$9yi8vxw0v-5r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
